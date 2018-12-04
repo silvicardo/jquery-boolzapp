07c906b97dac059545cfcb3ec4305cc86e350e5e
silvicardo <silvicardo@gmail.com>
2018-12-04 12:51:59 +0100

    contact list with search engine

---
+++ dataManager.js
@@ -0,0 +1,35 @@
+
+// creazione dei dati
+var riccardoSilvi = {
+  fullName: 'Riccardo Silvi',
+};
+var emanueleMazzante = {
+  fullName: 'Emanuele Mazzante',
+};
+var michelePapagni = {
+  fullName: 'Michele Papagni',
+};
+var fabioForghieri = {
+  fullName: 'Fabio Forghieri',
+};
+var javascriptLanguage = {
+  fullName: 'Javascript',
+};
+var chiaraPassaro = {
+  fullName: 'Chiara Passaro',
+};
+var paolaPoggini = {
+  fullName: 'Paola Poggini',
+};
+var giuseppeBarbara = {
+  fullName: 'Giuseppe Barbara',
+};
+var gianlucaBianco = {
+  fullName: 'Matteo Pelosi',
+};
+var diegoAneli = {
+  fullName: 'Diego Aneli',
+};
+
+var appUser = riccardoSilvi;
+var contactsDatabase = [diegoAneli, gianlucaBianco, giuseppeBarbara, paolaPoggini, chiaraPassaro, javascriptLanguage, fabioForghieri, michelePapagni, emanueleMazzante];
--- main.js
+++ main.js
@@ -14,6 +14,9 @@ var messagesArea = $('.messages_area');
 var userMessageTemplate = $('.message.user_message.template');
 var contactMessageTemplate = $('.message.contact_message.template');
 
+/********* AREA MESSAGGI ********/
+
+
 //alla prima digitazione nel campo input appare il tasto invia
 //e il tasto microfono viene nascosto
 inputField.keypress(function () {
@@ -30,14 +33,76 @@ sendMessageBtn.click(function () {
   addNewMessageFrom(inputField.val(), userMessageTemplate, messagesArea);
   inputField.val('');
   sendMessageBtn.hide();
-  micBtn.show();
-  addNewMessageFrom('Ok', contactMessageTemplate, messagesArea);
+  setTimeout(function () {
+    addNewMessageFrom('Ok', contactMessageTemplate, messagesArea);
+    micBtn.show();
+  }, 1000);
 });
-
 
 function addNewMessageFrom(text, templateMessage, tagToAppend) {
   var template = templateMessage.clone();
   template.children('.message_text').text(text);
   template.removeClass('template');
   tagToAppend.append(template);
+}
+
+/************ AREA RICERCA ******************/
+var inputRicerca = $('#search_bar_input');
+var contactsArea = $('.contacts_list');
+var activeContacts = contactsDatabase;
+var contactTemplate = $('.contact.template');
+
+console.log(activeContacts);
+
+//Al primo lancio mostra i contatti da cui si hanno dei messaggi
+manageContactsListFrom(activeContacts, contactTemplate, contactsArea);
+
+//poi ad ogni rilascio di un tasto con l'input selezionato
+inputRicerca.on({
+  keyup: function () {
+    removeAllContactsFromList();
+    var searchResult = getContactsFrom(activeContacts, inputRicerca.val());
+    console.log(searchResult);
+    manageContactsListFrom(searchResult, contactTemplate, contactsArea);
+  },
+});
+
+function getContactsFrom(database, searchParameter) {
+
+  var result = [];
+  if (searchParameter != null) {
+    for (var i = 0; i < database.length; i++) {
+      if (database[i].fullName.includes(searchParameter)) {
+        result.push(database[i]);
+      }
+    }
+
+    return result;
+  }
+  //returns all activeContacts if there is no parameter
+  return database;
+}
+
+function manageContactsListFrom(contactGroup, contactTemplate, tagToAppend) {
+  for (var i = 0; i < contactGroup.length; i++) {
+    var contact = contactTemplate.clone();
+    var contactName = contact.find('.contact_name');
+    var latestMessage = contact.find('.last_message');
+    latestMessage.text('Check messaggio');
+    contactName.text(contactGroup[i].fullName);
+    contact.removeClass('template');
+    contact.addClass('real');
+    if (i == 0) {
+      contact.addClass('selected');
+    }
+
+    tagToAppend.append(contact);
+  }
+
+}
+
+function removeAllContactsFromList(){
+  $('.contact.real').each(function () {
+    $(this).remove();
+  });
 }
